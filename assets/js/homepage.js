6254c63d3b0b30de5aeecef24a10b92ac4a98497
Viet Anh Nguyen <vietanh@vietanhdev.com>
2017-03-19 09:48:06 +0700

    update website

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	function helloWorld(){
 		$("#typed").typed({
 			strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Viet Anh.", "Nice to meet you!"],
-			cursorChar: " |",
+			cursorChar: " &#9749;",
 			typeSpeed: 45,
 			callback: function(){
 		    	setTimeout(function(){
