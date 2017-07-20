9ba7f57b9df81662395136fd7794276928a01d24
khanhsaker97 <khanhsaker1997@gmail.com>
2017-07-20 16:57:35 +0700

    sua site

--- _site/assets/js/homepage.js
+++ _site/assets/js/homepage.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('*[verlang="vi"]').hide();
 	function helloWorld(){
 		$("#typed").typed({
-			strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Viet Anh.", "Nice to meet you!"],
+			strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Duy Khanh.", "Nice to meet you!"],
 			cursorChar: " &#9749;",
 			typeSpeed: 45,
 			callback: function(){
